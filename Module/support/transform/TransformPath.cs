dd63ccf4b88d6b43d2a7f4f4916aa1a818e5244e
Oscar Virot <virot@virot.com>
2024-07-01 00:54:44 +0200

    Added Transform/Validate to Paths Add Import-YubikeyPIV to allow import PrivateKeys P12 Added Pester tests
    ECDsaCng is giving issues with Key export

---
+++ Module/support/transform/TransformPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Management.Automation;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using Yubico.YubiKey.Piv;
+
+
+namespace powershellYK.support.transform
+{
+    class TransformPath : System.Management.Automation.ArgumentTransformationAttribute
+    {
+
+        public override object Transform(EngineIntrinsics engineIntrinsics, object inputData)
+        {
+            if (inputData is string)
+            {
+                // there is a bug with GetCurrentDirectory, which breaks relative paths
+                var myPowersShellInstance = PowerShell.Create(RunspaceMode.CurrentRunspace).AddCommand("Resolve-Path");
+                myPowersShellInstance.AddParameter("Path", inputData);
+                myPowersShellInstance.AddCommand("Select-Object").AddParameter("ExpandProperty", "Path");
+                System.Collections.ObjectModel.Collection<System.Management.Automation.PSObject> returnValue = myPowersShellInstance.Invoke();
+                string? newPath = returnValue.FirstOrDefault()?.BaseObject.ToString();
+                if (System.IO.File.Exists(newPath))
+                {
+                    return newPath;
+                }
+            }
+            return inputData;
+        }
+    }
+}
---
+++ Module/support/validators/ValidatePath.cs
@@ -0,0 +1,31 @@
+﻿using System.Management.Automation;
+using System.Security;
+using Yubico.YubiKey.Otp;
+
+namespace powershellYK.support.validators
+{
+    public class ValidatePath : ValidateArgumentsAttribute
+    {
+        public string _fileExt { get; } = "";
+        protected override void Validate(object arguments, EngineIntrinsics engineIntrinsics)
+        {
+            if (!Path.Exists((string)arguments))
+            {
+                throw new ArgumentException($"Path {arguments} does not exist.");
+            }
+            if (!((string)arguments).EndsWith(_fileExt))
+            {
+                throw new ArgumentException($"Path {arguments} needs to end with {_fileExt}.");
+            }
+            return;
+        }
+        public ValidatePath(string fileExt)
+        {
+            this._fileExt = fileExt;
+        }
+        public ValidatePath()
+        {
+        }
+    }
+
+}
