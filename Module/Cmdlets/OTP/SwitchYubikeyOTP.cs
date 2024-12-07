59ba7d82d9f97d431191f2341dcfc62102d5a42f
J.L.M <57787248+JMarkstrom@users.noreply.github.com>
2024-12-07 14:29:43 +0100

    Update SwitchYubikeyOTP.cs

--- Module/Cmdlets/OTP/SwitchYubikeyOTP.cs
+++ Module/Cmdlets/OTP/SwitchYubikeyOTP.cs
@@ -12,7 +12,7 @@ namespace powershellYK.Cmdlets.OTP
         {
             if (YubiKeyModule._yubikey is null)
             {
-                WriteDebug("No Yubikey selected, calling Connect-Yubikey");
+                WriteDebug("No YubiKey selected, calling Connect-Yubikey");
                 try
                 {
                     var myPowersShellInstance = PowerShell.Create(RunspaceMode.CurrentRunspace).AddCommand("Connect-Yubikey");
