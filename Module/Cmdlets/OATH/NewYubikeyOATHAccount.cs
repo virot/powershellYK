8504000d4886ffb4c5de0e0d0e8ca750d0e27c21
J.L.M <57787248+JMarkstrom@users.noreply.github.com>
2024-12-07 14:35:43 +0100

    Update NewYubikeyOATHAccount.cs

--- Module/Cmdlets/OATH/NewYubikeyOATHAccount.cs
+++ Module/Cmdlets/OATH/NewYubikeyOATHAccount.cs
@@ -55,7 +55,7 @@ namespace powershellYK.Cmdlets.OATH
                 WriteDebug("No YubiKey selected, calling Connect-Yubikey");
                 var myPowersShellInstance = PowerShell.Create(RunspaceMode.CurrentRunspace).AddCommand("Connect-Yubikey");
                 myPowersShellInstance.Invoke();
-                WriteDebug($"Successfully connected.");
+                WriteDebug($"Successfully connected");
             }
         }
 
