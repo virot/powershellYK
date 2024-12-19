cd92b7c983c2925856894221f481fc702b4b7712
Oscar Virot <virot@virot.com>
2024-12-19 23:21:12 +0100

    .NET YubiKey SDK 1.12.1 (#75)
    * Upgrade  .NET YubiKey SDK to 1.12.1
    
    * Added Cmdlet **Enable-powershellYKSDKLogging** to enable SDK logging information to the Console.

---
+++ Module/Cmdlets/Other/Enable-PowershellYKSDKLogging.cs
@@ -0,0 +1,15 @@
+﻿using System.Management.Automation;
+using Microsoft.Extensions.Logging;
+using Yubico.Core.Logging;
+
+namespace powershellYK.Cmdlets.Other
+{
+    [Cmdlet(VerbsLifecycle.Enable, "powershellYKSDKLogging")]
+    public class Enable_PowershellYKSDKLoggingCmdlet : Cmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            Log.ConfigureLoggerFactory(builder => builder.AddConsole());
+        }
+    }
+}
