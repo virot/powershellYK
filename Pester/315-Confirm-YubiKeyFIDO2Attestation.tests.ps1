7c74a374537c46cf72d33e6b6c92317153609347
Oscar Virot <virot@virot.com>
2026-03-18 23:11:02 +0100

    Update pester 315 to newer version commands

--- Pester/315-Confirm-YubiKeyFIDO2Attestation.tests.ps1
+++ Pester/315-Confirm-YubiKeyFIDO2Attestation.tests.ps1
@@ -1,51 +1,51 @@
 Describe "Confirm-YubiKeyFIDO2Attestation paths" -Tag "Without-YubiKey" {
     It -Name "Verify 'Confirm-YubiKeyFIDO2Attestation -AttestationObject _fullfile_' works, Windows" -Skip:(!$IsWindows) -Test {
         $pest_return = Confirm-YubiKeyFIDO2Attestation -AttestationObject "$PSScriptRoot\TestData\attestation.bin"
-        $pest_return.GetType().FullName | Should Be 'powershellYK.FIDO2.FIDO2AttestationResult'
-        $pest_return.AttestationValidated | Should Be $true
-        $pest_return.AttestationPath | Should Not Be $null
-        ($pest_return.AttestationPath.Count -ge 1) | Should Be $true
+        $pest_return.GetType().FullName | Should -Be 'powershellYK.FIDO2.FIDO2AttestationResult'
+        $pest_return.AttestationValidated | Should -Be $true
+        $pest_return.AttestationPath | Should Not -Be $null
+        ($pest_return.AttestationPath.Count -ge 1) | Should -Be $true
     }
 
     It -Name "Verify 'Confirm-YubiKeyFIDO2Attestation -AttestationObject _fullfile_' works, Non-Windows" -Skip:($IsWindows) -Test {
         $pest_return = Confirm-YubiKeyFIDO2Attestation -AttestationObject "$PSScriptRoot/TestData/attestation.bin"
-        $pest_return.GetType().FullName | Should Be 'powershellYK.FIDO2.FIDO2AttestationResult'
-        $pest_return.AttestationValidated | Should Be $true
-        $pest_return.AttestationPath | Should Not Be $null
+        $pest_return.GetType().FullName | Should -Be 'powershellYK.FIDO2.FIDO2AttestationResult'
+        $pest_return.AttestationValidated | Should -Be $true
+        $pest_return.AttestationPath | Should Not -Be $null
     }
 
     It -Name "Verify 'Confirm-YubiKeyFIDO2Attestation -AttestationObject _shortfile_' works, Windows" -Skip:(!$IsWindows) -Test {
         $pest_return = Confirm-YubiKeyFIDO2Attestation -AttestationObject ".\Pester\TestData\attestation.bin"
-        $pest_return.GetType().FullName | Should Be 'powershellYK.FIDO2.FIDO2AttestationResult'
-        $pest_return.AttestationValidated | Should Be $true
+        $pest_return.GetType().FullName | Should -Be 'powershellYK.FIDO2.FIDO2AttestationResult'
+        $pest_return.AttestationValidated | Should -Be $true
     }
 
     It -Name "Verify 'Confirm-YubiKeyFIDO2Attestation -AttestationObject _shortfile_' works, Non-Windows" -Skip:($IsWindows) -Test {
         $pest_return = Confirm-YubiKeyFIDO2Attestation -AttestationObject "./Pester/TestData/attestation.bin"
-        $pest_return.GetType().FullName | Should Be 'powershellYK.FIDO2.FIDO2AttestationResult'
-        $pest_return.AttestationValidated | Should Be $true
+        $pest_return.GetType().FullName | Should -Be 'powershellYK.FIDO2.FIDO2AttestationResult'
+        $pest_return.AttestationValidated | Should -Be $true
     }
 }
 
 Describe "Confirm-YubiKeyFIDO2Attestation output" -Tag 'Dry' {
     It -Name "Verify AttestationPath contains Yubico root" -Test {
         $pest_return = Confirm-YubiKeyFIDO2Attestation -AttestationObject "$PSScriptRoot\TestData\attestation.bin"
-        ($pest_return.AttestationPath -join ' ') | Should Match 'Yubico'
+        ($pest_return.AttestationPath -join ' ') | Should -Match 'Yubico'
     }
 }
 
 Describe "Confirm-YubiKeyFIDO2Attestation Errors" -Tag 'Dry' {
     It -Name "Missing file throws" -Test {
         $badPath = Join-Path $PSScriptRoot "TestData\nonexistent_attestation_315_test.bin"
-        (Test-Path $badPath) | Should Be $false
+        (Test-Path $badPath) | Should -Be $false
         $threw = $false
         try { Confirm-YubiKeyFIDO2Attestation -AttestationObject $badPath } catch { $threw = $true }
-        $threw | Should Be $true
+        $threw | Should -Be $true
     }
 
     It -Name "Invalid format throws" -Test {
         $threw = $false
         try { Confirm-YubiKeyFIDO2Attestation -AttestationObject "$PSScriptRoot\TestData\rsa_2048_cert.pem" } catch { $threw = $true }
-        $threw | Should Be $true
+        $threw | Should -Be $true
     }
 }
